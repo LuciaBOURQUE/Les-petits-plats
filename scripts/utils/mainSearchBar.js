d5373bcf09adf0a3ab7a2977fcf2ece7f9fece73
LuciaBOURQUE <lucia.bourque@orange.fr>
2022-10-11 17:51:10 +0200

    test unitaire dans la bar de recherche

--- scripts/utils/mainSearchBar.js
+++ scripts/utils/mainSearchBar.js
@@ -3,8 +3,11 @@
 
 
 /* Tableau des recettes triées par l'input global */
+
 let BtnMainSearch = document.querySelector('.btn-search');
 BtnMainSearch.addEventListener('click', () => {
+    const start = performance.now()
+
     const arrayInputRecipe = [];
     let researchMonitor = document.getElementById('search').value;
 
@@ -44,4 +47,58 @@ BtnMainSearch.addEventListener('click', () => {
         }
     })
     displayCardRecipe(arrayInputRecipe);
+
+    const end = performance.now()
+    console.log(`Resultat ${end - start} milliseconds`);
 })
+
+/*
+let BtnMainSearch = document.querySelector('.btn-search');
+BtnMainSearch.addEventListener('click', () => {
+    console.time(doSomething);
+
+    function doSomething (){   
+        const arrayInputRecipe = [];
+        let researchMonitor = document.getElementById('search').value;
+
+        recipes.forEach((recipe) => {
+            let doublon = false;  
+            
+            let names = recipe.name;    
+            if (names.toLowerCase().includes(researchMonitor.toLowerCase()) && doublon == false ) {
+                arrayInputRecipe.push(recipe);
+                doublon = true;
+            }
+
+            let ingredients = recipe.ingredients;
+            ingredients.forEach((ingredient) => {
+                let ingredientsTabList = ingredient.ingredient;
+                if (ingredientsTabList.toLowerCase().includes(researchMonitor.toLowerCase()) && doublon == false) {
+                    arrayInputRecipe.push(recipe);
+                    doublon = true;
+                }
+            })
+
+            let appliances = recipe.appliance;
+            if (appliances.toLowerCase().includes(researchMonitor.toLowerCase()) && doublon == false ) {
+                arrayInputRecipe.push(recipe);
+                doublon = true;
+            }
+
+            let ustensils = recipe.ustensils;
+            if (doublon == false ) {
+                ustensils.forEach((ustensil) => {
+                    let ustensilsTabList = ustensil;
+                    if (ustensilsTabList.toLowerCase().includes(researchMonitor.toLowerCase()) ) {
+                        arrayInputRecipe.push(recipe);
+                        doublon = true;
+                    }
+                })
+            }
+        })
+    displayCardRecipe(arrayInputRecipe);
+    }
+    doSomething();
+
+    console.timeEnd(doSomething);
+})*/
