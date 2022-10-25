d5454855651bdef0ce4efdf8a91a3f59cf67b676
LuciaBOURQUE <lucia.bourque@orange.fr>
2022-10-25 10:59:30 +0200

    fichier modifié

--- scripts/utils/mainSearchBarV2.js
+++ scripts/utils/mainSearchBarV2.js
@@ -1,54 +1,66 @@
-/* Fonctionnalité de recherche : performant et rapide
-   2e VERSION - Méthode Objet Array (FOREACH) */
-
-/* Tableau des recettes triées par l'input global*/
-let BtnMainSearch = document.querySelector('.btn-search');
-BtnMainSearch.addEventListener('click', () => {
+/* 2e VERSION - Méthode Objet Array (FOREACH) */
+function eventMainSearch() {
     const arrayInputRecipe = [];
     let researchMonitorValue = document.getElementById('search').value;
 
     if (researchMonitorValue.length <= 3 || researchMonitorValue.length == "" ) {
-        document.querySelector(".error-message");
-    }
+        document.querySelector(".error-message-letter").style.display = "block";
+        return false;
+    } else {
+        document.querySelector(".error-message-letter").style.display = "none";
 
-    recipes.forEach((recipe) => {
-        let doublon = false;  
+        recipes.forEach((recipe) => {
+            let doublon = false;
         
-        let names = recipe.name;    
-        if (names.toLowerCase().includes(researchMonitorValue.toLowerCase()) && doublon == false ) {
-            arrayInputRecipe.push(recipe);
-            doublon = true;
-        }
-
-        let ingredients = recipe.ingredients;
-        ingredients.forEach((ingredient) => {
-            let ingredientsTabList = ingredient.ingredient;
-            if (ingredientsTabList.toLowerCase().includes(researchMonitorValue.toLowerCase()) && doublon == false) {
+            let names = recipe.name;    
+            if (names.toLowerCase().includes(researchMonitorValue.toLowerCase()) && doublon == false ) {
                 arrayInputRecipe.push(recipe);
                 doublon = true;
-            }
-        });
-
-        let appliances = recipe.appliance;
-        if (appliances.toLowerCase().includes(researchMonitorValue.toLowerCase()) && doublon == false ) {
-            arrayInputRecipe.push(recipe);
-            doublon = true;
-        }
-
-        let ustensils = recipe.ustensils;
-        if (doublon == false ) {
-            ustensils.forEach((ustensil) => {
-                let ustensilsTabList = ustensil;
-                if (ustensilsTabList.toLowerCase().includes(researchMonitorValue.toLowerCase()) ) {
+            } /*else {
+                document.querySelector(".error-message-recipe").style.display = "block";
+            }*/
+    
+            let ingredients = recipe.ingredients;
+            ingredients.forEach((ingredient) => {
+                let ingredientsTabList = ingredient.ingredient;
+                if (ingredientsTabList.toLowerCase().includes(researchMonitorValue.toLowerCase()) && doublon == false) {
                     arrayInputRecipe.push(recipe);
                     doublon = true;
                 }
             });
-        }
-    });
-    displayCardRecipe(arrayInputRecipe);
+    
+            let appliances = recipe.appliance;
+            if (appliances.toLowerCase().includes(researchMonitorValue.toLowerCase()) && doublon == false ) {
+                arrayInputRecipe.push(recipe);
+                doublon = true;
+            }
+    
+            let ustensils = recipe.ustensils;
+            if (doublon == false ) {
+                ustensils.forEach((ustensil) => {
+                    let ustensilsTabList = ustensil;
+                    if (ustensilsTabList.toLowerCase().includes(researchMonitorValue.toLowerCase()) ) {
+                        arrayInputRecipe.push(recipe);
+                        doublon = true;
+                    }
+                });
+            }
+        });
+        displayCardRecipe(arrayInputRecipe);
+        return true;
+    }
+}
+
+let BtnMainSearch = document.querySelector('.btn-search');
+BtnMainSearch.addEventListener('click', () => {
+    eventMainSearch();
 });
 
+BtnMainSearch.addEventListener('keydown', (e) => {
+    if(e.key === "Enter") {
+        eventMainSearch();
+    }
+});
 
 
 
