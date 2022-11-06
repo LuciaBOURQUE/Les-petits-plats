bb50c75ad42d4a33ead421ec4e2bd4cd72e7d5fe
LuciaBOURQUE <lucia.bourque@orange.fr>
2022-11-06 09:39:55 +0100

    fichier index modifié

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,20 @@
 
 /* Fonction affichage des cartes de recettes
    avec appel de la Factory "Card Recipe" */
-function displayCardRecipe(arrayInputRecipe) {
+   function displayCardRecipe(arrayInputRecipe) {
     const cardRecipeSection = document.querySelector(".section-grid-cards");
     cardRecipeSection.innerHTML= "";
 
-    arrayInputRecipe.forEach((recipe) => {
-        const cardRecipeModel = factoryCardRecipe(recipe);
-        const recipeCardDOM = cardRecipeModel.getCardRecipeDOM();
-        cardRecipeSection.appendChild(recipeCardDOM);
-    })
+    if (arrayInputRecipe.length == 0) {
+        document.querySelector(".error-message-recipe").style.display = "block";
+    } else {
+        arrayInputRecipe.forEach((recipe) => {
+            document.querySelector(".error-message-recipe").style.display = "none";
+            const cardRecipeModel = factoryCardRecipe(recipe);
+            const recipeCardDOM = cardRecipeModel.getCardRecipeDOM();
+            cardRecipeSection.appendChild(recipeCardDOM);
+        });
+    }
+
 }
 displayCardRecipe(recipes);
