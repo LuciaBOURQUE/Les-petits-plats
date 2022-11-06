7e157db843c067efe7fa19ec5ce9f316df324588
LuciaBOURQUE <lucia.bourque@orange.fr>
2022-11-06 10:05:05 +0100

    ajout d'un nouveau fichier js searchBar

---
+++ scripts/utils/mainSearchBar.js
@@ -0,0 +1,62 @@
+/* 1er VERSION - Méthode boucle native (FOR) */
+function eventMainSearch() {
+    const arrayInputRecipe = [];
+    let researchMonitor = document.getElementById('search').value;
+
+    if(researchMonitor.length < 3 || researchMonitor.length == ""){
+        document.querySelector(".error-message-letter").style.display = "block";
+        document.querySelector(".error-message-recipe").style.display = "";
+        displayCardRecipe(recipes);
+        return false;
+    } else {
+        document.querySelector(".error-message-letter").style.display = "none";
+
+        for (let i = 0; i<recipes.length; i++){
+            let doublon = false;
+    
+            let names = recipes[i].name;
+            if (names.toLowerCase().includes(researchMonitor.toLowerCase()) && doublon == false ) {
+                arrayInputRecipe.push(recipes[i]);
+                doublon = true;
+            }
+    
+            let ingredients = recipes[i].ingredients;
+            for(let i = 0; i<ingredients.length; i++) {
+                let ingredientsTabList = ingredients[i].ingredient;
+                if (ingredientsTabList.toLowerCase().includes(researchMonitor.toLowerCase()) && doublon == false) {
+                    arrayInputRecipe.push(recipes[i]);
+                    doublon = true;
+                }
+            }
+    
+            let appliances = recipes[i].appliance;
+            if (appliances.toLowerCase().includes(researchMonitor.toLowerCase()) && doublon == false ) {
+                arrayInputRecipe.push(recipes[i]);
+                doublon = true;
+            }
+    
+            let ustensils = recipes[i].ustensils;
+            for (let i = 0; i<ustensils.length; i++) {
+                let ustensilsTabList = ustensils[i];
+                if (ustensilsTabList.toLowerCase().includes(researchMonitor.toLowerCase()) && doublon == false ) {
+                    arrayInputRecipe.push(recipes[i]);
+                    doublon = true;
+                }
+            }
+    
+        };
+        displayCardRecipe(arrayInputRecipe);
+        return true;
+    }
+}
+
+let BtnMainSearch = document.querySelector('.btn-search');
+BtnMainSearch.addEventListener('click', () => {
+    eventMainSearch();
+});
+
+BtnMainSearch.addEventListener('keydown', (e) => {
+    if (e.key === "Enter"){
+        eventMainSearch();
+    }
+});
